ea5e497deedd69771390bdf180feb5068a5319cb
Ardi S <ardisa2711@gmail.com>
2024-03-24 01:39:50 +0800

    Perubahan pada header. sudah  selesai

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const navbar = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".hamburger");
+const lines = document.querySelectorAll(".line");
+const menuItems = document.querySelectorAll(".nav-list li");
+
+hamburger.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  lines.forEach(line => line.classList.toggle("hidden"));
+  lines[0].classList.toggle("rotate-right");
+  lines[2].classList.toggle("rotate-left");
+});
+
+menuItems.forEach(item => {
+  item.addEventListener("click", function () {
+    navbar.classList.remove("active");
+    lines.forEach(line => line.classList.remove("hidden", "rotate-right", "rotate-left"));
+  });
+});
